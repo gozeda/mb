ab9f0de42372287bb6139f5f1147121e3077db5e
yinzheda <yinzheda@gmail.com>
2017-09-29 17:20:39 +0800

    add NOW

--- helloworld.cs
+++ helloworld.cs
@@ -5,7 +5,7 @@ class HelloWorld
     static void Main()
     {
 #if DebugConfig
-        Console.WriteLine("WE ARE IN THE DEBUG CONFIGURATION");
+        Console.WriteLine("WE ARE IN THE DEBUG CONFIGURATION NOW");
 #endif
 
         Console.WriteLine("Hello, world!");
